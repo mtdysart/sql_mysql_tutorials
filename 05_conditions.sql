00d6f161aa8a44d86dcf4205025c1ecb602a9823
Matt <matt.dysart66@gmail.com>
2020-09-01 10:26:54 -0500

    Finished conditions section

--- 05_conditions.sql
+++ 05_conditions.sql
@@ -48,3 +48,40 @@ WHERE hire_date LIKE("2000%");
 SELECT * 
 FROM employees
 WHERE emp_no LIKE("_1000");
+
+# Note: BETWEEN is inclusive
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN "1990-01-01" AND "2000-01-01";
+
+SELECT *
+FROM salaries
+WHERE salary BETWEEN 66000 AND 70000;
+
+SELECT *
+FROM employees
+WHERE emp_no NOT BETWEEN 10004 AND 10012;
+
+SELECT dept_name
+FROM departments
+WHERE dept_no BETWEEN "d003" AND "d006";
+
+SELECT *
+FROM employees
+WHERE first_name IS NULL;
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IS NOT NULL;
+
+SELECT * 
+FROM employees
+WHERE hire_date > "2000-01-01";
+
+SELECT *
+FROM employees
+WHERE gender = "F" AND hire_date >= "2000-01-01";
+
+SELECT salary
+FROM salaries
+WHERE salary > 150000;
